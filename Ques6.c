9c02123a16ff4bb380571b19a722a5de99421fed
sakshig19 <126548290+sakshig19@users.noreply.github.com>
2024-10-14 14:22:04 +0530

    Add files via upload

---
+++ Ques6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int m, n;
+    printf("Enter the value of m: ");
+    scanf("%d", &m);
+    if(m>0){
+        n = 1;
+        printf("1");
+        if(m=0){
+        printf("0"); }
+    }
+    else{
+        printf("-1");
+    }
+    return 0;
+}
