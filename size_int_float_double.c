e4bb22c6e3bb5965266e9556d6211d2d06b1278e
sakshig19 <126548290+sakshig19@users.noreply.github.com>
2024-11-07 01:45:52 +0530

    Add files via upload

---
+++ size_int_float_double.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a;
+    float f;
+    double d;
+    printf("The size of integer is %d \n",sizeof(a));
+    printf("The size of float is %d \n",sizeof(f));
+    printf("The size of double is %d \n",sizeof(d));
+    return 0;
+}
